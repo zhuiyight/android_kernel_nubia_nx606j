4a2eb7555881d007803e03dae5312915d054d96c
Mark Rutland <mark.rutland@arm.com>
2018-04-12 12:11:24 +0100

    arm/arm64: KVM: Consolidate the PSCI include files
    From: Marc Zyngier <marc.zyngier@arm.com>
    
    commit 1a2fb94e6a771ff94f4afa22497a4695187b820c upstream.
    
    As we're about to update the PSCI support, and because I'm lazy,
    let's move the PSCI include file to include/kvm so that both
    ARM architectures can find it.
    
    Acked-by: Christoffer Dall <christoffer.dall@linaro.org>
    Tested-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    [v4.9: account for files moved to virt/ upstream]
    Signed-off-by: Mark Rutland <mark.rutland@arm.com> [v4.9 backport]
    Tested-by: Greg Hackmann <ghackmann@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm/kvm/psci.c
+++ arch/arm/kvm/psci.c
@@ -27,6 +27,8 @@
 
 #include <kvm/arm_psci.h>
 
+#include <uapi/linux/psci.h>
+
 /*
  * This is an implementation of the Power State Coordination Interface
  * as described in ARM document number ARM DEN 0022A.
